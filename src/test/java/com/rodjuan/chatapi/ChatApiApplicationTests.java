ea735d36574e7ceabc681a6af8bfef328befca30
rodjuan06 <ja.rodriguezc2006@gmail.com>
2026-09-02 00:50:16 -0300

    Add database integration tests with Testcontainers

--- src/test/java/com/rodjuan/chatapi/ChatApiApplicationTests.java
+++ src/test/java/com/rodjuan/chatapi/ChatApiApplicationTests.java
@@ -2,12 +2,25 @@ package com.rodjuan.chatapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChatApiApplicationTests {
 
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17-alpine");
+
+    @Container
+    @ServiceConnection
+    static final MongoDBContainer mongo = new MongoDBContainer("mongo:8.0.4");
+
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
